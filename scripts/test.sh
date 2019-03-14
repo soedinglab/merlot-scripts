833337386525bfdc6cbcc77273bec67cf0c4d485
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2019-03-14 16:39:56 +0100

    prepping for bash submission

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,4 +9,4 @@
 #BSUB -R cbscratch
 #BSUB -R "span[ptile=16]"
 
-time Rscript /home/users/npapado/bin/merlot-scripts/scripts/benchmark_slingshot.R -o /cbscratch/npapado/resample/benchmark1 -j sim3 -d 2 -i /cbscratch/npapado/resample/benchmark1/sim3/sim3_destiny_log_k -t /home/users/npapado/bin/merlot-scripts; 2>> /cbscratch/npapado/resample/benchmark1/sim3/sim3.exec
+time Rscript /home/users/npapado/bin/merlot-scripts/scripts/benchmark_slingshot.R -o /cbscratch/npapado/resample/benchmark5 -j sim0 -d 6 -i /cbscratch/npapado/resample/benchmark5/sim0/sim0_destiny_log_k -t /home/users/npapado/bin/merlot-scripts; 2>> /cbscratch/npapado/resample/benchmark5/sim0/sim0.exec
