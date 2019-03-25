21aa857f28a086fc36334a972e10e3becdcd3ce4
Nikolaos Papadopoulos <nikolaspapadop@yahoo.com>
2019-03-25 19:09:32 +0100

    fixed computationally singular issue

--- scripts/examine_sling.R
+++ scripts/examine_sling.R
@@ -1,10 +1,16 @@
 library(igraph)
 library(mclust, quietly = TRUE)
 library(slingshot)
+
+various <- paste(mscripts, "/scripts/various.R", sep = "")
+evaluat <- paste(mscripts, "/scripts/evaluate_method.R", sep = "")
+
+suppressPackageStartupMessages(source("~/Documents/repos/merlot-scripts/scripts/evaluate_method.R"))
+suppressPackageStartupMessages(source("~/Documents/repos/merlot-scripts/scripts/various.R"))
 source("~/Documents/repos/merlot-scripts/scripts/clusters_to_branches.R")
 
 benchmark <- "benchmark5"
-sim <- "sim0"
+sim <- "sim68"
 job <- paste("~/Documents/data/examine", benchmark, sim, sim, sep="/")
 
 dm <- readRDS(paste(job, "destiny_log_k", sep="_"))
@@ -13,33 +19,49 @@ cell_params <- read.table(file = paste(job, "cellparams.txt", sep = "_"), sep = 
 time <- cell_params$pseudotime - min(cell_params$pseudotime) + 1
 start <- min(which(time == min(time)))
 labels <- cell_params$branches + 1
+par_loc <- paste(job, "params.txt", sep = "_")
+N <- dim(CellCoordinates)[1]
+K <- dim(CellCoordinates)[2]
 
-# mclusters <- mclustBIC(CellCoordinates, G = 5:50)
-# mod1 <- Mclust(CellCoordinates, x = mclusters)
-# 
-# # use clusters to get lineages
-# # specify the cluster of cell 1 as start to get best pseudotime results
-# sds <- getLineages(CellCoordinates, mod1$classification, start.clus = mod1$classification[start])
-# 
-# # get connectivity of clusters and re-create the MST
-# nodes_order <- order(as.numeric(colnames(sds@adjacency)))
-# adj_matrix <- sds@adjacency[nodes_order, nodes_order]
-# mstree <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected")
-# 
-# # map cells to clusters, get cluster centroids and collapse co-linear clusters
-# # to form branch assignments for each cell
-# cells2nodes <- mod1$classification
-# centroids <- t(mod1$parameters$mean)
-# clusterid <- as.numeric(cells2nodes)
-# sling_branches <- assign_branches(mstree, clusterid, centroids, CellCoordinates)
-# 
-# # predict pseudotime per trajectory for each cell
-# sds <- getCurves(sds)
-# sling <- sds
+mclusters <- mclustBIC(CellCoordinates, G = 5:50)
+mod1 <- Mclust(CellCoordinates, x = mclusters)
 
-which_sling <- "slingshot_destiny_log_k"
-sling <- readRDS(paste(job, which_sling, sep="_"))
-interpreted <- read.table(paste(job, which_sling, "branches.csv", sep="_"), sep=",", header = T)
+# use clusters to get lineages
+# specify the cluster of cell 1 as start to get best pseudotime results
+sds <- tryCatch( {
+  getLineages(CellCoordinates, mod1$classification, start.clus = mod1$classification[start])
+  }, 
+  error = function(cond) {
+    print("System is computationally singular. Rerunning with some random noise.")
+    print(cond)
+    # LOG_MESSAGE <- paste(LOG_MESSAGE, cond, "\n")
+    # assign("LOG_MESSAGE", LOG_MESSAGE, envir = .GlobalEnv)
+    span <- range(CellCoordinates)[2] - range(CellCoordinates)[1]
+    noise <- rnorm(N*K, mean = 0, sd = span/100)
+    dim(noise) <- c(N, K)
+    getLineages(CellCoordinates+noise, mod1$classification, start.clus = mod1$classification[start])
+  })
+
+# get connectivity of clusters and re-create the MST
+nodes_order <- order(as.numeric(colnames(sds@adjacency)))
+adj_matrix <- sds@adjacency[nodes_order, nodes_order]
+mstree <- graph_from_adjacency_matrix(adj_matrix, mode = "undirected")
+
+# map cells to clusters, get cluster centroids and collapse co-linear clusters
+# to form branch assignments for each cell
+cells2nodes <- mod1$classification
+centroids <- t(mod1$parameters$mean)
+clusterid <- as.numeric(cells2nodes)
+sling_branches <- assign_branches(mstree, clusterid, centroids, CellCoordinates)
+adjusted_mi(sling_branches, labels, "bla")
+
+# predict pseudotime per trajectory for each cell
+sds <- getCurves(sds)
+sling <- sds
+
+# which_sling <- "slingshot_destiny_log_k"
+# sling <- readRDS(paste(job, which_sling, sep="_"))
+# interpreted <- read.table(paste(job, which_sling, "branches.csv", sep="_"), sep=",", header = T)
 
 predicted_labels <- as.factor(apply(sling@clusterLabels, 1, function(x) names(which(x == 1))))
 
